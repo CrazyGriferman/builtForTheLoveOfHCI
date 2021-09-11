c4cd73b312876075267c1c8d5438af3537cc1138
Crazygriferman <251031557@qq.com>
2021-09-11 10:35:37 +0800

    MainPage horizontal scroll demo complete

--- src/views/Main/MainPage/MainPage.js
+++ src/views/Main/MainPage/MainPage.js
@@ -1,10 +1,15 @@
-import React from 'react';
-
+import React from "react";
+import "./MainPage.css";
 
 export function MainPage() {
-    return (
-        <div>
-            hello
-        </div>
-    );
+  return (
+    <div class="outer-wrapper">
+      <div class="wrapper">
+        <div class="slide one"> </div>
+        <div class="slide two"> </div>
+        <div class="slide three"> </div>
+        <div class="slide four"> </div>
+      </div>
+    </div>
+  );
 }

d15da44058dc810e296116ff555b905981bd43f1
Crazygriferman <251031557@qq.com>
2021-09-19 10:11:16 +0800

    change: designing page strcture <section/>

--- src/views/DesigningWithTheMindInMind/DesigningWithTheMindInMindPage/DesigningWithTheMindInMindPage.js
+++ src/views/DesigningWithTheMindInMind/DesigningWithTheMindInMindPage/DesigningWithTheMindInMindPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router";
+import pureBrownBackground from "../../../assets/pure-brown-background.jpg";
 import designingImg from "../../../assets/designing-with-the-mind-in-mind.jpeg";
 import "./DesigningWithTheMindInMindPage.css";
 
@@ -13,27 +14,41 @@ function DesigningWithTheMindInMindPage() {
   }, [location]);
 
   return (
-    <div className="designingbook designingbook-is-active ">
-      <div className="designingpages">
-        <div className="designingpage  "></div>
-        <div className="designingpage flipped ">
-          <img className="designingpageCover" src={designingImg} alt="" />
-        </div>
-        <div className="designingpage ">
-          <div
-            className="designingchapter"
-            onClick={() => {
-              console.log(1);
-              RouterHistory.push(
-                "/main/designingwiththemindinmind/chaptertwosimilarityfivestar"
-              );
-            }}
-          >
-            chaptertwosimilarityfivestar
+    <>
+      <section>
+        <img
+          className="designingbackground"
+          src={pureBrownBackground}
+          alt="background"
+        />
+        <div className="designingbook designingbook-is-active ">
+          <div className="designingpages">
+            <div className="designingpage  "></div>
+            <div className="designingpage flipped ">
+              <img className="designingpageCover" src={designingImg} alt="" />
+            </div>
+            <div className="designingpage ">
+              <div
+                className="designingchapter"
+                onClick={() => {
+                  console.log(1);
+                  RouterHistory.push(
+                    "/main/designingwiththemindinmind/chaptertwosimilarityfivestar"
+                  );
+                }}
+              >
+                chaptertwosimilarityfivestar
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+      <section>
+        <p>432543534</p>
+        <h2>5423</h2>
+        <p>4234235</p>
+      </section>
+    </>
   );
 }
 

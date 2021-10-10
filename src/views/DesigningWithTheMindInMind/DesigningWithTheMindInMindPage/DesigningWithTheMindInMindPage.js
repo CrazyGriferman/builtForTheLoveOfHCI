c142cf5945a8a11e5568344a2b0d18e20e87a427
Crazygriferman <251031557@qq.com>
2021-10-11 00:57:51 +0800

    update: convert DesigningWithTheMindInMindPage.css to scss file

--- src/views/DesigningWithTheMindInMind/DesigningWithTheMindInMindPage/DesigningWithTheMindInMindPage.js
+++ src/views/DesigningWithTheMindInMind/DesigningWithTheMindInMindPage/DesigningWithTheMindInMindPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router";
 import pureBrownBackground from "../../../assets/pure-brown-background.jpg";
 import designingImg from "../../../assets/designing-with-the-mind-in-mind.jpeg";
-import "./DesigningWithTheMindInMindPage.css";
+import "./DesigningWithTheMindInMindPage.scss";
 
 function DesigningWithTheMindInMindPage(props) {
   const RouterHistory = useHistory();
@@ -15,34 +15,32 @@ function DesigningWithTheMindInMindPage(props) {
 
   return (
     <>
-      <section>
-        <img
-          className="designingbackground"
-          src={pureBrownBackground}
-          alt="background"
-        />
-        <div className="designingbook designingbook-is-active ">
-          <div className="designingpages">
-            <div className="designingpage  "></div>
-            <div className="designingpage flipped ">
-              <img className="designingpageCover" src={designingImg} alt="" />
-            </div>
-            <div className="designingpage ">
-              <div
-                className="designingchapter"
-                onClick={() => {
-                  console.log(1);
-                  RouterHistory.push(
-                    "/main/designingwiththemindinmind/chaptertwosimilarityfivestar"
-                  );
-                }}
-              >
-                test
-              </div>
+      <img
+        className="designingbackground"
+        src={pureBrownBackground}
+        alt="background"
+      />
+      <div className="designingbook  designingbook-is-active">
+        <div className="designingpages">
+          <div className="designingpage  "></div>
+          <div className="designingpage flipped ">
+            <img className="designingpageCover" src={designingImg} alt="" />
+          </div>
+          <div className="designingpage ">
+            <div
+              className="designingchapter"
+              onClick={() => {
+                console.log(1);
+                RouterHistory.push(
+                  "/main/designingwiththemindinmind/chaptertwosimilarityfivestar"
+                );
+              }}
+            >
+              test
             </div>
           </div>
         </div>
-      </section>
+      </div>
       {props.children}
     </>
   );

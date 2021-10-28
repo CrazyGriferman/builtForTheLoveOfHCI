67b5e55a02d5ee27ca08585abde05c9702893d9d
Crazygriferman <251031557@qq.com>
2021-10-28 21:38:17 +0800

    add: carousel demo reference link: https://medium.com/suyeonme/react-how-to-build-an-image-slider-with-hooks-in-react-decd4a3ab152

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ROUTES, { RenderRoutes } from "./router/router";
 import { DesigningWithTheMindInMindChapterTwelve } from "./views/DesigningWithTheMindInMind/DesigningWithTheMindInMindChapterTwelve/DesigningWithTheMindInMindChapterTwelve";
 import "./App.scss";
+import { Test } from "./components/DesigningWithTheMindInMindComponents/FMSC/Test";
 
 /* function App() {
   return (
@@ -10,8 +11,12 @@ import "./App.scss";
   );
 } */
 
-function App() {
+/* function App() {
   return <DesigningWithTheMindInMindChapterTwelve />;
+} */
+
+function App() {
+  return <Test />;
 }
 
 export default App;
---
+++ src/components/DesigningWithTheMindInMindComponents/FMSC/Test.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Test.scss";
+import {
+  oldBookWithLight,
+  oldBookPage,
+  pureBrownBackground,
+} from "../../../assets/index";
+
+export function Test() {
+  const [index, setIndex] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [xPosition, setXPosition] = useState(0);
+
+  const slideRef = useRef();
+
+  useEffect(() => {
+    if (slideRef.current) {
+      const width = slideRef.current.clientWidth;
+      setWidth(width);
+    }
+  }, [setWidth]);
+
+  const handleClickPrev = () => {
+    if (index === 0) return;
+    setIndex(index - 1);
+    setXPosition(xPosition + width);
+  };
+
+  const handleClickNext = () => {
+    //1，2，3，4 判断4到1的情况
+    if (index === images.length - 1) {
+      setIndex(0);
+      setXPosition(0);
+    } else {
+      setIndex(index + 1);
+      setXPosition(xPosition - width);
+    }
+  };
+
+  /* const images = [
+    "https://blog-1300038350.cos.ap-shanghai.myqcloud.com/blogImage/20211013215608.png",
+    "https://blog-1300038350.cos.ap-shanghai.myqcloud.com/blogImage/20211014195308.png",
+    "https://blog-1300038350.cos.ap-shanghai.myqcloud.com/blogImage/20211012110356.png",
+  ]; */
+
+  const images = [oldBookWithLight, oldBookPage, pureBrownBackground];
+
+  return (
+    <div className="test">
+      <div className="test-carousel">
+        <div
+          className="test-carousel_slide"
+          style={{
+            transition: "transform 0.6s ease-in-out",
+            transform: `translateX(${xPosition}px)`,
+          }}
+          ref={slideRef}
+        >
+          {images.map((img, i) => (
+            <img src={img} alt="test" key={i} />
+          ))}
+        </div>
+        <button className="test-carousel_button left" onClick={handleClickPrev}>
+          53534
+        </button>
+        <button
+          className="test-carousel_button right"
+          onClick={handleClickNext}
+        >
+          42355
+        </button>
+      </div>
+    </div>
+  );
+}
